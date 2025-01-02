7cb97907743052b298cdb84d2d17ada823307e48
ahmadhidayatt <comp.ahmadhidayat@gmail.com>
2025-01-02 23:24:46 +0700

    Update multitoolbox.sh

--- multitoolbox.sh
+++ multitoolbox.sh
@@ -560,8 +560,9 @@ function install_node(){
 		echo -e "${WORNING}${CYAN}Docker is not working correct or is not installed.${NC}"
 		exit
 	fi
+	echo -e "curl -s https://raw.githubusercontent.com/RunOnFlux/fluxnode-multitool/${ROOT_BRANCH}/install_pro.sh"	
 	bash -i <(curl -s https://raw.githubusercontent.com/RunOnFlux/fluxnode-multitool/${ROOT_BRANCH}/install_pro.sh)
-	echo -e "curl -s https://raw.githubusercontent.com/RunOnFlux/fluxnode-multitool/${ROOT_BRANCH}/install_pro.sh"
+
 }
 function install_docker(){
 	echo -e "${GREEN}Module: Install Docker${NC}"
